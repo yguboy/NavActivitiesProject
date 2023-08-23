d30cee3b74b826323653867ab4966a0044148fd0
Ygor <ygorespada14@gmail.com>
2023-08-23 11:29:28 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/up/navactivities/PrimeiraActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.up.navactivities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PrimeiraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primeira)
+    }
+}
---
+++ app/src/main/java/br/edu/up/navactivities/TerceiraActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.up.navactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TerceiraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terceira)
+    }
+}
